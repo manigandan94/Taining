364938a663e53ee01ca015db8159f13e5d006eb6
daisytraining01 <daisytraining01@gmail.com>
2020-05-06 16:30:32 +0530

    aqua

---
+++ TeamAqua_Manigandan/src/TraingAqua/Employee1.java
@@ -0,0 +1,30 @@
+package TraingAqua;
+
+public class Employee1 {
+    private String name;
+    private int Empno;
+    
+    public String getName() 
+    {
+        return name;
+     }     
+    public int getEmpno() 
+    {
+    return Empno;
+    }
+    public void setName(String newName) 
+      {
+      name = newName;
+      }
+     public void setEmpno(int Rollno)
+      {
+          Empno = Rollno;
+      }
+}
+
+/* output:
+
+Constructor Method
+EmpName:Mani
+Rollno:2959
+*/
